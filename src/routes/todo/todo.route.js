08c5b9ceef4c48288973d951d74d8464e2509f45
Elite007 <alijonoveldorbek78@gmail.com>
2025-02-23 01:19:31 +0900

    sdv

--- src/routes/todo/todo.route.js
+++ src/routes/todo/todo.route.js
@@ -7,6 +7,6 @@ export const todoRouter = Router();
 todoRouter.get('/get', getAllTodo)  //getAllTodo ni export qilamiz
 todoRouter.post('/add', AddTodo)
 //todoRouter.post('/add/:id', getById)   // / dan keyin istalgan id ga boshlangan amal qilsa boladi
-todoRouter.put('/update', updateTodo)   //put butun boshli malumotlarni yangilaydi
+todoRouter.put('/update/:todoID', updateTodo)   //put butun boshli malumotlarni yangilaydi
 //todoRouter.patch('/update', AddTodo)   //patch qisqa malumotlarni yangilaydi, kop ishlatilmaydi
 todoRouter.delete('/delete', deleteTodo)
